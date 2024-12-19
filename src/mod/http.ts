5b928cac5c99db0c6bd54d3ec541b62da65b668a
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2024-12-19 13:58:16 +0200

    feat: Add HTTP module and update exports in index

---
+++ src/mod/http.ts
@@ -0,0 +1,13 @@
+// import * as http from 'http';
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.end('Welcome to the server');
+//   }
+//   if (req.url === '/about') {
+//     res.end('About us');
+//   }
+//   res.end('<a href="/">home</a> or <a href="/ about">about</a>');
+// });
+
+// server.listen(5001);
--- src/mod/index.ts
+++ src/mod/index.ts
@@ -4,6 +4,7 @@ const names = require('./names');
 const os = require('./os');
 const path = require('./path');
 const file = require('./file');
+const http = require('./http');
 
 module.exports = {
   mg,
@@ -12,4 +13,5 @@ module.exports = {
   os,
   path,
   file,
+  http,
 };
