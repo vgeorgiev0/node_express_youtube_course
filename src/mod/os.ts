fefd3aab5df56668901a7b0639743b3165e0238c
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2024-12-19 13:31:34 +0200

    refactor: Comment out OS module usage and related logging

--- src/mod/os.ts
+++ src/mod/os.ts
@@ -1,19 +1,19 @@
-import * as os from 'os';
+// import * as os from 'os';
 
-// info about the current user
+// // info about the current user
 
-const user = os.userInfo();
-// console.log(user);
+// const user = os.userInfo();
+// // console.log(user);
 
-// method returns the system uptime in seconds
-const uptime = os.uptime();
-// console.log(uptime);
+// // method returns the system uptime in seconds
+// const uptime = os.uptime();
+// // console.log(uptime);
 
-// info about the current operating system
-const osInfo = {
-  os: os.type(),
-  release: os.release(),
-  totalMem: os.totalmem(),
-  freeMem: os.freemem(),
-};
-// console.log(osInfo);
+// // info about the current operating system
+// const osInfo = {
+//   os: os.type(),
+//   release: os.release(),
+//   totalMem: os.totalmem(),
+//   freeMem: os.freemem(),
+// };
+// // console.log(osInfo);
