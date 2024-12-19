b49d844c2ffbb704a0b0d9d46a8247040eef252b
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2024-12-19 09:59:01 +0200

    feat: Add asynchronous code and improve logging function interface

--- src/mod/async.ts
+++ src/mod/async.ts
@@ -1,20 +1,21 @@
-import { log } from '../util/log';
+// import { log } from '../util/log';
+// import * as fs from 'fs';
 
 //setTimeout
-setTimeout(() => {
-  console.log(1);
-}, 1000);
+// setTimeout(() => {
+//   console.log(1);
+// }, 1000);
 
-// nested setTimeouts
-setTimeout(() => {
-  console.log(2);
-  setTimeout(() => {
-    console.log(3);
-    setTimeout(() => {
-      console.log(4);
-    }, 1000);
-  }, 1000);
-}, 1000);
+// // nested setTimeouts
+// setTimeout(() => {
+//   console.log(2);
+//   setTimeout(() => {
+//     console.log(3);
+//     setTimeout(() => {
+//       console.log(4);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
 
 // button event handler in browser
 
@@ -32,22 +33,30 @@ setTimeout(() => {
 //   console.log(data);
 // });
 
-const myPromise = new Promise((resolve, reject) => {
-  const randomNumber = Math.floor(Math.random() * 2);
-  if (randomNumber === 0) {
-    resolve('Success! Random number is 0!');
-  } else {
-    reject(`Random number is ${randomNumber} and it's not 0!`);
-  }
-});
+// const myPromise = new Promise((resolve, reject) => {
+//   const randomNumber = Math.floor(Math.random() * 2);
+//   if (randomNumber === 0) {
+//     resolve('Success! Random number is 0!');
+//   } else {
+//     reject(`Random number is ${randomNumber} and it's not 0!`);
+//   }
+// });
 
-myPromise
-  .then((data) => {
-    log({ message: data, background: 'purple', color: 'yellow' });
-  })
-  .catch((err) => {
-    log({ message: err, background: 'purple', color: 'yellow' });
-  });
+// myPromise
+//   .then((data) => {
+//     log({
+//       type: 'bg',
+//       message: data,
+//       background: 'green',
+//     });
+//   })
+//   .catch((err) => {
+//     log({
+//       type: 'bg',
+//       message: err,
+//       background: 'red',
+//     });
+//   });
 
 // fs readFile with promises
 // fs.promises
@@ -62,3 +71,55 @@ myPromise
 //       message: err,
 //     });
 //   });
+
+//fetch with promises
+// fetch('https://jsonplaceholder.typicode.com/posts/1').then((response) => {
+//   response.json().then((data) => {
+//     log({
+//       message: data,
+//       type: 'text',
+//     });
+//   });
+// }
+// ).catch((err) => {
+//   log({
+//     message: err,
+//     type: 'text',
+//   });
+// });
+
+// async/await
+
+// load file with async/await
+
+// (async () => {
+//   try {
+//     const data = await fs.promises.readFile('src/constants/text.txt', {
+//       encoding: 'utf-8',
+//     });
+//     log({
+//       message: data,
+//       type: 'text',
+//       color: 'cyan',
+//     });
+//   } catch (error) {
+//     log({
+//       message: error,
+//       type: 'text',
+//       color: 'red',
+//     });
+//   }
+// })();
+
+// // async/await with fetch
+// const fetchData = async (id: number) => {
+//   try {
+//     const response = await fetch(
+//       `https://jsonplaceholder.typicode.com/posts/${id}`
+//     );
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
--- src/util/log.ts
+++ src/util/log.ts
@@ -1,5 +1,3 @@
-let prefix = '\u001b[1;';
-let clear = '\u001b[0m';
 let colorCode = {
   red: '1',
   green: '2',
@@ -8,23 +6,40 @@ let colorCode = {
   purple: '5',
   cyan: '6',
 };
-let FgdStrCodVar = '';
-let BgdStrCodVar = '';
 
-export function log({
-  message,
-  color = 'red',
-  background = 'red',
-}: {
+interface LogOptions {
   message: unknown;
+}
+
+interface LogOptionsWithColor extends LogOptions {
   color?: keyof typeof colorCode;
+  type: 'text';
+}
+
+interface LogOptionsWithBackground extends LogOptions {
   background?: keyof typeof colorCode;
-}) {
-  if (color && colorCode[color]) {
-    FgdStrCodVar = prefix + '3' + colorCode[color] + 'm';
+  type: 'bg';
+}
+
+type LogOptionsWithColorAndBackground =
+  | LogOptionsWithColor
+  | LogOptionsWithBackground;
+
+export function log(params: LogOptionsWithColorAndBackground) {
+  let prefix = '\u001b[1;';
+  let clear = '\u001b[0m';
+  let text = '';
+  let backgroundColor = '';
+
+  const { message, type } = params;
+
+  if (type === 'bg' && params.background) {
+    backgroundColor = prefix + '4' + colorCode[params.background] + 'm';
   }
-  if (background && colorCode[background]) {
-    BgdStrCodVar = prefix + '4' + colorCode[background] + 'm';
+
+  if (type === 'text' && params.color) {
+    text = prefix + '3' + colorCode[params.color] + 'm';
   }
-  console.log(FgdStrCodVar, BgdStrCodVar, message, clear);
+
+  console.log(text, backgroundColor, message, clear);
 }
